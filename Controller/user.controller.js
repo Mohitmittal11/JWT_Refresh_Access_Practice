afd8c60d5ba43d4e768c8063b125e4bdf31523cf
Mohit Mittal <mohit.mittal24@tecorb.co>
2024-09-09 19:35:31 +0530

    comment out all console

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -11,13 +11,15 @@ const addUserData = async (req, res) => {
     const isUserExist = await userModel.findOne({ email: requestBody?.email });
     // console.log("Is user exist is ", isUserExist);
     if (isUserExist !== null) {
+      // throw new Error("User is not Exist");
       res.status(409).json({ message: "User is Already Exist" });
     } else {
       const hash = bcrypt.genSaltSync(12);
+      // console.log("Hash Password is ", hash);
       const hashedPass = bcrypt.hashSync(requestBody?.password, hash);
       // console.log("comparable password is ", comparablePass);
       requestBody.password = hashedPass;
-      console.log("User info to save data in database is ", requestBody);
+      // console.log("User info to save data in database is ", requestBody);
       const saveUserDatabase = await userModel.create(req.body);
       if (saveUserDatabase) {
         res.status(200).json({
@@ -34,16 +36,25 @@ const addUserData = async (req, res) => {
 
 const getUserData = async (req, res) => {
   try {
+    // throw new Error("You does not have access to get the User Data");
     const userId = req.params.id;
-    console.log("User id is", userId);
     const getUserData = await userModel.findOne({ _id: userId });
-    if (getUserData === null || userId === undefined) {
+    // console.log("Users Data for get is", getUserData);
+    if (
+      !getUserData?.username ||
+      getUserData?.username === undefined ||
+      getUserData?.username === null
+    ) {
       res.status(404).json({ message: "User Does not Exist" });
     } else {
       const token = jwt.sign({ _id: userId }, process.env.JWT_SECRET_KEY, {
         expiresIn: "10m",
       });
-      console.log("Token is ", token);
+      if (token) {
+        // const isTrue = jwt.verify(getUserData.password, token);
+        // console.log("Data get from user modal is ", token);
+      }
+      // console.log("Token is ", token);
       res.cookie("access_token", token).status(200).json({
         statusCode: 200,
         message: "Data Found Successfully",
@@ -57,12 +68,11 @@ const getUserData = async (req, res) => {
 };
 
 const refreshToken = async (req, res) => {
-  console.log("hiii");
   const access_Token = await req?.headers.cookie;
-  console.log("Accesss token is ", access_Token.slice(13, access_Token.length));
+  // console.log("Accesss token is ", access_Token.slice(13, access_Token.length));
   const refreshToken = access_Token.slice(13, access_Token.length);
   const isVerified = jwt.verify(refreshToken, process.env.JWT_SECRET_KEY);
-  console.log("Is Verfied Data ", isVerified);
+  // console.log("Is Verfied Data ", isVerified);
   if (isVerified && isVerified !== undefined && isVerified !== null) {
     const accessToken = jwt.sign(
       { _id: isVerified?._id },
